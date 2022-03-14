cdaea4581f619692517261c1e3f223d026769cb7
jrfep <jr.ferrer.paris@gmail.com>
2022-03-14 11:02:43 +1100

    add tables

--- psql/02-litrev-categorical.sql
+++ psql/02-litrev-categorical.sql
@@ -91,3 +91,22 @@ CREATE TABLE IF NOT EXISTS litrev.surv4 (
   weight_notes text[],
   FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TYPE seed_dormancy_vocabulary AS ENUM ('PY','PD','PY-PD','MPD','MD','ND');
+
+
+CREATE TABLE IF NOT EXISTS litrev.germ8 (
+  record_id SERIAL PRIMARY KEY,
+  species VARCHAR(255),
+  species_code VARCHAR(10),
+  main_source VARCHAR(50) ,
+  original_sources text[],
+  norm_value seed_dormancy_vocabulary,
+  method_of_estimation method_vocabulary,
+  raw_value text[],
+  original_notes text[],
+  additional_notes text[],
+  weight smallint NOT NULL DEFAULT 1,
+  weight_notes text[],
+  FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
