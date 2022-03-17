e51fb2816c8a5c11321077e7b4a3ced0664192d9
jrfep <jr.ferrer.paris@gmail.com>
2022-03-17 11:00:50 +1100

    more priority traits

--- psql/02-litrev-fuzzy-numbers.sql
+++ psql/02-litrev-fuzzy-numbers.sql
@@ -81,3 +81,63 @@ CREATE TABLE IF NOT EXISTS litrev.grow1 (
   CHECK (best >= lower AND best <= upper),
   FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+CREATE TABLE IF NOT EXISTS litrev.surv5 (
+  record_id SERIAL PRIMARY KEY,
+  species VARCHAR(255),
+  species_code VARCHAR(10),
+  main_source VARCHAR(50) ,
+  original_sources text[],
+  best int,
+  lower int,
+  upper int,
+  method_of_estimation method_vocabulary,
+  raw_value text[],
+  original_notes text[],
+  additional_notes text[],
+  weight smallint NOT NULL DEFAULT 1,
+  weight_notes text[],
+  CHECK (best >= lower AND best <= upper),
+  FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS litrev.surv6 (
+  record_id SERIAL PRIMARY KEY,
+  species VARCHAR(255),
+  species_code VARCHAR(10),
+  main_source VARCHAR(50) ,
+  original_sources text[],
+  best int,
+  lower int,
+  upper int,
+  method_of_estimation method_vocabulary,
+  raw_value text[],
+  original_notes text[],
+  additional_notes text[],
+  weight smallint NOT NULL DEFAULT 1,
+  weight_notes text[],
+  CHECK (best >= lower AND best <= upper),
+  FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS litrev.surv7 (
+  record_id SERIAL PRIMARY KEY,
+  species VARCHAR(255),
+  species_code VARCHAR(10),
+  main_source VARCHAR(50) ,
+  original_sources text[],
+  best int,
+  lower int,
+  upper int,
+  method_of_estimation method_vocabulary,
+  raw_value text[],
+  original_notes text[],
+  additional_notes text[],
+  weight smallint NOT NULL DEFAULT 1,
+  weight_notes text[],
+  CHECK (best >= lower AND best <= upper),
+  FOREIGN KEY (main_source) REFERENCES litrev.ref_list (ref_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
